d01aa8b27777a628455193ff4e0792486484e8a6
huara <47011132+huara@users.noreply.github.com>
2019-01-31 15:34:20 -0500

    Copyright Date Added

--- src/ca/sheridancollege/week1/softwarefundamental/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamental/StudentTester.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Date: January 31st 2019
  */
 package ca.sheridancollege.week1.softwarefundamental;
 
